ad58347a07309f4597892d9bada2dc408e63a396
Patrick Corbett <patrick@crowdlab.com>
2017-03-08 16:44:54 +0000

    Add react

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const NullComponent = () => null;
+const React = require('react');
+
+const NullComponent = () => <div />;
 
 module.exports = function() {
 	this.cacheable();
+	console.log('1', NullComponent)
 	return NullComponent;
 };
 module.exports.pitch = function() {
 	this.cacheable();
+	console.log('2', NullComponent)
 	return NullComponent;
 };
